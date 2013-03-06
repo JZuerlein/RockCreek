01fdd10a14c90b4e78bd6422f93aeef11b46e733
Jeff Zuerlein <Jeff@Zuerlein.com>
2013-03-06 10:23:28 -0600

    Adding some logging.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-
+      puts "Ready to redirect to subdomain."
       redirect_to root_url(:subdomain => user.tenant.subdomain) + "dashboard/show.html"
     else
       flash.now.alert = "Invalid email or password"
