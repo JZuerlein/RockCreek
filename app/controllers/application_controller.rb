f7db84dc8d267b8b9d55758a35541e787b8dc0f1
Jeff Zuerlein <Jeff@Zuerlein.com>
2013-03-06 10:32:38 -0600

    Adding some logging.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,11 @@ class ApplicationController < ActionController::Base
   def current_user
     ## @ means it is an instance variable
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if (@current_user != nil)
+      puts "current_user is : " + @current_user.email
+      puts "session[:user_id] : " + session[:user_id]
+    end
+
   end
   helper_method :current_user
 
