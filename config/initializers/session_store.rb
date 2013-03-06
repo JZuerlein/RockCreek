31e98bacb779e638264a87f9a26a0ee5b37aadc2
Jeff Zuerlein <Jeff@Zuerlein.com>
2013-03-06 10:14:18 -0600

    Modified the session_store.rb file to point to mychangecontrol domain.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-RockCreek::Application.config.session_store :cookie_store, key: '_RockCreek_session', :domain => "lvh.me"
-
+#RockCreek::Application.config.session_store :cookie_store, key: '_RockCreek_session', :domain => "lvh.me"
+RockCreek::Application.config.session_store :cookie_store, key: '_RockCreek_session', :domain => "mychangecontrol"
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
