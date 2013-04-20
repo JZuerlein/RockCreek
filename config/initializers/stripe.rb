e482fce4f96ccacb245d3d156dee8221367ac43f
Jeff Zuerlein <Jeff@Zuerlein.com>
2013-04-19 20:50:02 -0500

    Trying to fix Stripe again.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 if Rails.env.to_s == "production"
-  Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+  Stripe.api_key = sk_test_qHdeDWtJDQbCA1sVZRNpeKGn
 else
-  Stripe.api_key = "boot"
+  Stripe.api_key = sk_test_qHdeDWtJDQbCA1sVZRNpeKGn
 end
 STRIPE_PUBLIC_KEY = "pk_test_cZ8D0YdtAYofPno7oQ0iDWhu"
